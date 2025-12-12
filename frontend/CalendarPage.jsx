759c5306a05b08cf6d07d644b2da4d5f0a0ddba0
itc251gna <itc.251gna@gmail.com>
2025-12-12 11:31:02 +0200

    Create CalendarPage.jsx

---
+++ frontend/CalendarPage.jsx
@@ -0,0 +1,11 @@
+import AppLayout from "../layouts/AppLayout";
+
+export default function CalendarPage() {
+  return (
+    <AppLayout>
+      <h1 className="text-2xl font-semibold text-slate-900 dark:text-white">
+        Ημερολόγιο Παρουσιών
+      </h1>
+    </AppLayout>
+  );
+}
