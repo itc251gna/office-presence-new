564d344032f518f8d1d632b7a7cb70aa1abad22b
itc251gna <itc.251gna@gmail.com>
2025-12-12 11:31:40 +0200

    Create AdminPage.jsx

---
+++ frontend/AdminPage.jsx
@@ -0,0 +1,11 @@
+import AppLayout from "../layouts/AppLayout";
+
+export default function AdminPage() {
+  return (
+    <AppLayout>
+      <h1 className="text-2xl font-semibold text-slate-900 dark:text-white">
+        Πίνακας Διαχείρισης
+      </h1>
+    </AppLayout>
+  );
+}
